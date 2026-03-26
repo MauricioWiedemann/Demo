352d134cc21b1dfaa3a0dd2a3c59fe9d2d0857d6
brianmachado18 <110497734+brianmachado18@users.noreply.github.com>
2026-03-26 20:48:14 -0300

    tetoca

--- app/src/test/java/com/example/demo/ui/TaskViewModelTest.kt
+++ app/src/test/java/com/example/demo/ui/TaskViewModelTest.kt
@@ -1,16 +1,39 @@
 package com.example.demo.ui
 
+import com.example.demo.MainDispatcherRule
+import com.example.demo.data.local.TaskEntity
 import com.example.demo.data.repository.TaskRepository
 import com.example.demo.ui.main.TaskViewModel
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
 
 class TaskViewModelTest {
 
+    @get:Rule
+    var mainDispatcherRule = MainDispatcherRule()
+
+    @Test
+    fun viewModel_initialStates() = runTest(){
+        val repository = mockk<TaskRepository>(relaxed = true)
+        val viewModel = TaskViewModel(repository)
+
+        assertTrue(viewModel.uiState.value.tasks.isEmpty())
+        assertFalse(viewModel.uiState.value.isLoading)
+        assertNull(viewModel.uiState.value.error)
+    }
+
     @Test
     fun addTask_setError(){
-        val repository = mockk<TaskRepository>()
+        val repository = mockk<TaskRepository>(relaxed = true)
         val viewModel = TaskViewModel(repository)
 
         viewModel.addTask(" ")
@@ -18,12 +41,25 @@ class TaskViewModelTest {
     }
 
     @Test
-    fun addTask_setCorrect(){
-        val repository = mockk<TaskRepository>()
+    fun addTaskAndReload_setValid() = runTest {
+        val repository = mockk<TaskRepository>(relaxed = true)
+
+        coEvery { repository.addTask("1 - Cocinar") } returns Unit
+        coEvery { repository.getTasks() } returns listOf(
+            TaskEntity(id = 1, title = "1 - Cocinar")
+        )
+
         val viewModel = TaskViewModel(repository)
 
         viewModel.addTask("1 - Cocinar")
-        assertEquals("La tarea no puede estar vacía", viewModel.uiState.value.error)
+        advanceUntilIdle()
+
+        coVerify(exactly = 1) { repository.addTask("1 - Cocinar") }
+        coVerify(exactly = 1) { repository.getTasks() }
+
+        assertEquals(1, viewModel.uiState.value.tasks.size)
+        assertEquals("1 - Cocinar", viewModel.uiState.value.tasks.first().title)
+        assertNull(viewModel.uiState.value.error)
     }
 
 }
